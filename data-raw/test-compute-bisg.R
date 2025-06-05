7978cb3a120e0e0ac7b8dd10025d4ffa182e888f
jgabry <jgabry@gmail.com>
2025-06-05 08:52:25 -0600

    clean up bisg tests

--- data-raw/test-compute-bisg.R
+++ data-raw/test-compute-bisg.R
@@ -4,14 +4,12 @@
 library(tidyverse)
 library(glue)
 
-path_root <- "/Users/jgabry/Desktop/tmp/voter_bisg/"
+path_root <- "/Users/jgabry/Desktop/tmp/caliBISG-data/bisg-test/"
 df_agg <- read_csv(glue("{path_root}wa_2020.csv"))
 df_cen_surs <- read_csv(glue("{path_root}df_surnames.csv"))
 df_cen_counties  <- read_csv(glue("{path_root}county_census_wa2020.csv"))
 df_cen_usa  <- read_csv(glue("{path_root}usa_census_2020.csv"))
 
-
-# handy column-name vectors
 CEN_R_GIVEN_SUR_COLS <- c(
   "cen_r_given_sur_nh_aian",  "cen_r_given_sur_nh_api",
   "cen_r_given_sur_nh_black", "cen_r_given_sur_hispanic",
@@ -28,8 +26,9 @@ BISG_CEN_COUNTY_COLS <- c(
   "bisg_cen_county_hispanic", "bisg_cen_county_nh_white", "bisg_cen_county_other"
 )
 
-# draw 10 random (surname, county) pairs
-df_rows <- df_agg %>% slice_sample(n = 10)
+# draw 100 random (surname, county) pairs
+set.seed(1001)
+df_rows <- df_agg %>% slice_sample(n = 100)
 df_name_county  <- df_rows %>% select(name, county)
 
 # merge surname & county factors
@@ -74,5 +73,20 @@ error <- max(abs(
 
 cat(sprintf("max error: %.4g\n", error))
 
-result <- df_rows[ , c("name", "county", BISG_CEN_COUNTY_COLS)]
-rm(list = setdiff(ls(), "result"))
+df_rows$state <- "WA"
+df_rows$year <- 2020
+result <- df_rows[ , c("name", "year", "state", "county", BISG_CEN_COUNTY_COLS)]
+
+# rename columns to match names that package uses
+result <- result %>%
+  rename(
+    bisg_aian = bisg_cen_county_nh_aian,
+    bisg_api = bisg_cen_county_nh_api,
+    bisg_black_nh = bisg_cen_county_nh_black,
+    bisg_hispanic = bisg_cen_county_hispanic,
+    bisg_white_nh = bisg_cen_county_nh_white,
+    bisg_other = bisg_cen_county_other
+  )
+dput(as.data.frame(result), file = "tests/testthat/bisg-test-answers/precomputed-bisg.R")
+rm(list = ls())
+
